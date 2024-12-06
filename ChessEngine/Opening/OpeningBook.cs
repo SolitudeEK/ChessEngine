20cb8c9a9092675b1a040c8710f2b7084ad5ab69
Egor Knyazev <egorknz@gmail.com>
2024-12-06 23:40:57 +0100

    Add base for endgame book

---
+++ ChessEngine/Opening/OpeningBook.cs
@@ -0,0 +1,18 @@
+﻿using ChessEngine.Position;
+
+namespace ChessEngine.Opening
+{
+    public class OpeningBook
+    {
+        private List<Move> _moves = new List<Move>();
+        public OpeningBook(string path)
+        {
+
+        }
+
+        public Tuple<Move, int> TryToFindMove(Position.Position position)
+        {
+            return null;
+        }
+    }
+}
